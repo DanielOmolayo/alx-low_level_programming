967407c8c40ad2b3872e26358cb37e2e6cfc8d1a
DanielOmolayo <omolayondaniel@gmail.com>
2023-08-14 07:49:49 -0700

    Print single base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main = Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char number = '0';
+    while (number <= '9')
+    {
+        putchar(number);
+        number++;
+    }
+    putchar('\n');
+    return (0);
+}
