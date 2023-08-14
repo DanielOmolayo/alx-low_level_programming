13a78bb1f39ad9c8b454444228b1d0c20c117b3b
DanielOmolayo <omolayondaniel@gmail.com>
2023-08-14 03:37:39 -0700

    Print negative or positive number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (a < 0) {
-		printf("The number is negative");
-	}else if (a > 0) {
-		printf("The number is positive");
-	}else if (a == 0) {
-		printf("The number is zero');
+	if (n < 0) {
+		printf("%d is negative \n", n);
+	}else if (n > 0) {
+		printf("%d is positive \n", n);
+	}else if (n == 0) {
+		printf("%d is zero \n", n);
 	}
 	return (0);
 }
